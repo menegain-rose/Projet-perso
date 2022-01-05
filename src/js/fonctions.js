fd8ddf16fce67ff271ab21df4cf655ec5a1cf561
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-01-05 14:46:02 +0100

    html-ammélioré

--- src/js/fonctions.js
+++ src/js/fonctions.js
@@ -7,42 +7,47 @@ function bases() {
     return animation.play;
 }
 
-function carrousel() {
-
-    let images = document.querySelectorAll('.imageC');
+function carrouselImages() {
+    var images = document.querySelectorAll('.image');
     let d = 2000;
     let delta = 1000;
 
-    images.forEach(function(img,ind) {
-        img.style.zIndex = images.length - ind;
+    images.forEach(function(img,indice) {
+        img.style.zIndex = images.length - indice;
     });
-    
-    let animC = anime.timeline({
+
+    let anim155 = anime.timeline({
         loop: true,
         delay: 0
     });
 
-    animC.add({
+    anim155.add({
         targets: images,
-        translateX: [{value: 500, duration: d},
-                     {value: '-500', durataion: 0, delay: function(img,ind) {
-                                        if (ind == 0) return 3*delta + 2*d;
-                                        if (ind == 1) return 2*delta + d;
-                                        if (ind == 2) return delta;
-                                        return 0;
-                                    }
-                     },
-                     {value: 0, duration: function(img,ind) {
-                                    if (ind == 3) return 0;
-                                    return d;
-                                }
-                     }],
-        easing: 'easeInOutSine',
+        translateX: [{value: '500', duration: d},
+                     {value: '-500', duration: 0, delay: function(img,ind) {
+                         if (ind == 0) return (7*delta)+(6*d);
+                         if (ind == 1) return (6*delta)+(5*d);
+                         if (ind == 2) return (5*delta)+(4*d);
+                         if (ind == 3) return (4*delta)+(3*d);
+                         if (ind == 4) return (3*delta)+(2*d);
+                         if (ind == 5) return (2*delta)+d;
+                         if (ind == 6) return delta;
+                         return 0;
+                     }},
+                     {value: '0', duration: function(img,ind) {
+                         if (ind == 4) return 0;
+                         return d;
+                     }}],
+        easing: 'linear',
         delay: function(img,ind) {
             if (ind == 0) return delta;
-            if (ind == 1) return 2*delta + d;
-            if (ind == 2) return 3*delta + 2*d;
-            return 4*delta + 3*d;
+            if (ind == 1) return (2*delta)+d;
+            if (ind == 2) return (3*delta)+(2*d);
+            if (ind == 3) return (4*delta)+(3*d);
+            if (ind == 4) return (5*delta)+(4*d);
+            if (ind == 5) return (6*delta)+(5*d);
+            if (ind == 6) return (7*delta)+(6*d);
+            return (8*delta)+(7*d);
         }
     });
 }
